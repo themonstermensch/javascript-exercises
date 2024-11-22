7e705eb0692f6847abbf46375c32f9fa81bcfc59
TheMonsterMensch <114268283+themonstermensch@users.noreply.github.com>
2024-11-22 10:25:57 -0800

    add solution to reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reverseString = "";
+    for(let i = 0; i < str.length; i++) {
+        reverseString += str[str.length - (i+1)]
+    }
+    return reverseString
 };
 
 // Do not edit below this line
