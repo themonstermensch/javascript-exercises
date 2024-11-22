5c2f19db6a683b17313dc08f3603ba0d567f254a
TheMonsterMensch <114268283+themonstermensch@users.noreply.github.com>
2024-11-22 11:15:01 -0800

    add sumAll solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    let newArray = array
+    for(let i = 1; i < arguments.length; i++) {
+        while(newArray.includes(arguments[i])) {
+            newArray.splice(newArray.indexOf(arguments[i]), 1);
+        }
+    }
+    return newArray
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(first, second) {
+    if(!Number.isInteger(first) || !Number.isInteger(second)) {
+        return "ERROR"
+    } else if (Math.sign(first) == -1 || Math.sign(second) == -1) {
+        return "ERROR"
+    }
+    let min = Math.min(first, second);
+    let max = Math.max(first, second);
+    let sum = 0;
+    for(let i = min; i < (max+1); i++) {
+        sum += i;
+    }
+    return sum
 };
 
 // Do not edit below this line
