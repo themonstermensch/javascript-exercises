bb1a25edac41b638b43e8043ad36110fc4a6bc36
TheMonsterMensch <114268283+themonstermensch@users.noreply.github.com>
2025-01-19 13:29:40 -0800

    add solution to problem 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(...arr) {
+  return arr.reduce((total, current) => total + current);
 };
 
-const subtract = function() {
-	
+const subtract = function(...arr) {
+  return arr.reduce((total, current) => total - current);
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, current) => total * current);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+  while(num > 1) {
+    total *= num;
+    num -= 1;
+  }
+  return total;
 };
 
 // Do not edit below this line
