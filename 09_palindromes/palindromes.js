cc66be521734c8ec8d26dc0d15c5cb3e55136dee
TheMonsterMensch <114268283+themonstermensch@users.noreply.github.com>
2025-01-20 09:43:36 -0800

    add solution to problem 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let letters = str.toLowerCase().match(/[a-zA-Z\d]/g);
+    for(let i = 0; i < letters.length; i++) {
+        if(letters[i] != letters[letters.length - (i + 1)]) {
+            return false
+        }
+    }
+    return true
 };
 
 // Do not edit below this line
