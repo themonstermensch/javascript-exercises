6b966aacd10b93afc11d3247c111aca0c4a7e2a4
TheMonsterMensch <114268283+themonstermensch@users.noreply.github.com>
2025-01-20 10:02:26 -0800

    add solution to problem 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(Number(num) < 0) {
+        return "OOPS";
+    } else if (Number(num) == 0) {
+        return 0;
+    }
+    let previousvalue = 0;
+    let value = 1;
+    for(let i = 1; i < Number(num); i++) {
+        let newvalue = value + previousvalue
+        previousvalue = value
+        value = newvalue
+        //console.log("newvalue", value, "newprevious", previousvalue);
+    }
+    return value
 };
+
+//console.log(fibonacci(0))
 
 // Do not edit below this line
 module.exports = fibonacci;
